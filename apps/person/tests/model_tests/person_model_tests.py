654ad1e72a296203bd8cbec46105b9025abf8b9e
Pavel Khramov <paul.khramov@gmail.com>
2025-02-08 18:25:16 +0500

    Сделал тест модели классом

--- apps/person/tests/model_tests/person_model_tests.py
+++ apps/person/tests/model_tests/person_model_tests.py
@@ -2,19 +2,21 @@ import pytest
 
 from apps.person.tests.factories.person import PersonFactory
 
+class TestPersonModel:
+    """Тесты модели пользователя."""
 
-@pytest.mark.django_db
-def test_full_name():
-    """Проверка работы вычисляемого поля с ФИО пользователя."""
+    @pytest.mark.django_db
+    def test_full_name(self):
+        """Проверка работы вычисляемого поля с ФИО пользователя."""
 
-    persons = PersonFactory.create_batch(2)
-    another_person = PersonFactory.create(
-        patronymic='Ивановна',
-    )
-
-    for person in [*persons, another_person]:
-        full_name = (
-            f'{person.last_name} {person.first_name} {person.patronymic or ""}'
+        persons = PersonFactory.create_batch(2)
+        another_person = PersonFactory.create(
+            patronymic='Ивановна',
         )
 
-        assert person.full_name == full_name.strip()
+        for person in [*persons, another_person]:
+            full_name = (
+                f'{person.last_name} {person.first_name} {person.patronymic or ""}'
+            )
+
+            assert person.full_name == full_name.strip()
